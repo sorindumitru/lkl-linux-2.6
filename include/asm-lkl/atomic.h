7faaee9731c92c1534af7006798c30e9f3dee8ed
Octavian Purdila <tavi@cs.pub.ro>
2008-01-28 00:08:15 +0200

    Fix warning introduced by commit 7c7f2fdb5b44a3d4a16e17eed2e7ddb47b82725d.

--- include/asm-lkl/atomic.h
+++ include/asm-lkl/atomic.h
@@ -3,23 +3,23 @@
 
 #include <asm/system.h>
 
-typedef long atomic_t;
+typedef int atomic_t;
 
 #define ATOMIC_INIT(i) (i)
 
-static __inline__ long atomic_read(const atomic_t *v)
+static __inline__ int atomic_read(const atomic_t *v)
 {
 	return  *v;
 }
 
-static __inline__ void atomic_set(atomic_t *v, long l)
+static __inline__ void atomic_set(atomic_t *v, int i)
 {
-	*v=l;
+	*v=i;
 }
 
-static __inline__ void atomic_add(long l, atomic_t *v)
+static __inline__ void atomic_add(int i, atomic_t *v)
 {
-	*v+=l;
+	*v+=i;
 }
 
 static __inline__ void atomic_inc(atomic_t *v)
@@ -27,9 +27,9 @@ static __inline__ void atomic_inc(atomic_t *v)
 	return atomic_add(1, v);
 }
 
-static __inline__ void atomic_sub(long l, atomic_t *v)
+static __inline__ void atomic_sub(int i, atomic_t *v)
 {
-	return atomic_add(-l, v);
+	return atomic_add(-i, v);
 }
 
 static __inline__ void atomic_dec(atomic_t *v)
@@ -37,77 +37,77 @@ static __inline__ void atomic_dec(atomic_t *v)
 	return atomic_add(-1, v);
 }
 
-static __inline__ long atomic_add_and_test(long l, atomic_t *v)
+static __inline__ int atomic_add_and_test(int i, atomic_t *v)
 {
-	long ret;
+	int ret;
 	
-	*v+=l;
+	*v+=i;
 	ret=(*v == 0);
 	
 	return ret;
 }
 
-static __inline__ long atomic_sub_and_test(long l, atomic_t *v)
+static __inline__ int atomic_sub_and_test(int i, atomic_t *v)
 {
-	return atomic_add_and_test(-l, v);
+	return atomic_add_and_test(-i, v);
 }
 
-static __inline__ long atomic_dec_and_test(atomic_t *v)
+static __inline__ int atomic_dec_and_test(atomic_t *v)
 {
 	return atomic_add_and_test(-1, v);
 }
 
-static __inline__ long atomic_inc_and_test(atomic_t *v)
+static __inline__ int atomic_inc_and_test(atomic_t *v)
 {
 	return atomic_add_and_test(1, v);
 }
 
-static __inline__ long atomic_add_negative(long l, atomic_t *v)
+static __inline__ int atomic_add_negative(int i, atomic_t *v)
 {
-	long ret;
+	int ret;
 	
-	*v+=l;
+	*v+=i;
 	ret=(*v < 0);
 	
 	return ret;
 }
 
-static __inline__ long atomic_add_return(long l, atomic_t *v)
+static __inline__ int atomic_add_return(int i, atomic_t *v)
 {
-	long ret;
+	int ret;
 	
-	*v+=l;
+	*v+=i;
 	ret=*v;
 	
 	return ret;
 }
 
-static __inline__ long atomic_sub_return(long l, atomic_t *v)
+static __inline__ int atomic_sub_return(int i, atomic_t *v)
 {
-	return atomic_add_return(-l,v);
+	return atomic_add_return(-i,v);
 }
 
-static __inline__ long atomic_inc_return(atomic_t *v)
+static __inline__ int atomic_inc_return(atomic_t *v)
 {
 	return atomic_add_return(1,v);
 }
 
-static __inline__ long atomic_dec_return(atomic_t *v)
+static __inline__ int atomic_dec_return(atomic_t *v)
 {
 	return atomic_add_return(-1,v);
 }
 
-static inline long atomic_sub_if_positive(long l, atomic_t *v)
+static inline int atomic_sub_if_positive(int i, atomic_t *v)
 {
-	long ret;
+	int ret;
 	
-	ret=*v - l;
-	if (*v >= l)
-	    *v -= l;
+	ret=*v - i;
+	if (*v >= i)
+	    *v -= i;
 	return ret;
 }
 
-static __inline__ long atomic_dec_if_positive(atomic_t *v)
+static __inline__ int atomic_dec_if_positive(atomic_t *v)
 {
 	return atomic_sub_if_positive(1, v);
 }
@@ -121,9 +121,9 @@ static __inline__ long atomic_dec_if_positive(atomic_t *v)
  * Atomically adds @a to @v, so long as it was not @u.
  * Returns non-zero if @v was not @u, and zero otherwise.
  */
-static __inline__ long atomic_add_unless(atomic_t *v, long a, long u)
+static __inline__ int atomic_add_unless(atomic_t *v, int a, int u)
 {
-	long ret=1;
+	int ret=1;
 	
         if (*v != u) 
                 *v+=a;
@@ -132,7 +132,7 @@ static __inline__ long atomic_add_unless(atomic_t *v, long a, long u)
 	return ret;
 }
 
-static __inline__ long atomic_inc_not_zero(atomic_t *v)
+static __inline__ int atomic_inc_not_zero(atomic_t *v)
 {
         return atomic_add_unless(v, 1, 0);
 }
