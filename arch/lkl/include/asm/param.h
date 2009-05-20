6870a8004bbf4d61e99db391e32891dc8ca04ddb
Lucian Adrian Grijincu <lucian.grijincu@cs.pub.ro>
2009-05-20 20:02:19 +0300

    lkl: avoid overflows in kernel/time.c
    Tree-wide change done in bdc807871d58285737d50dc6163d0feb72cb0dc2.
    
    Fix: build failed because HZ was not passed to a build script:
      TIMEC   kernel/timeconst.h
      Usage: kernel/timeconst.pl HZ

--- arch/lkl/include/asm/param.h
+++ arch/lkl/include/asm/param.h
@@ -10,8 +10,8 @@
 #define MAXHOSTNAMELEN  64      /* max length of hostname */
 
 #ifdef __KERNEL__
-#define HZ 100
-#define USER_HZ	100	   /* .. some user interfaces are in "ticks" */
+#define HZ             CONFIG_HZ  /* internal timer frequency */
+#define USER_HZ	       100        /* .. some user interfaces are in "ticks" */
 #define CLOCKS_PER_SEC (USER_HZ)  /* frequency at which times() counts */
 #endif
 
